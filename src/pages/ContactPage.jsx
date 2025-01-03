7d93c930fe4e6d07d6c9bd608e29011d7b88f2aa
AkmalIhksan <akmalihksan181@gmail.com>
2025-01-03 21:31:51 +0700

    fix: contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "../components/header/Navbar";
+import ContactFooter from "../components/shared/ContactFooter";
+import Footer from "../components/footer/Footer";
 
 const Contact = () => {
   return (
     <>
       <Navbar showOnScroll={false} />
-      <div className="contact bg-blue-400 min-h-screen pt-[100px]">
-        Contact Page
-      </div>
+      <div className="contact bg-blue-400 pt-[100px]">Contact Page</div>
+      <ContactFooter />
+      <Footer />
     </>
   );
 };
