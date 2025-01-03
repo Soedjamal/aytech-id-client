d0ebcf366d6a924ec3ed7811a4e7d402b3fdc027
AkmalIhksan <akmalihksan181@gmail.com>
2025-01-03 21:53:12 +0700

    add: configuration css variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        firstBlue: "#0b529a",
+        secondBlue: "#0574bb",
+        thirdBlue: "#078cca",
+        fourthBlue: "#06a4cf",
+        fivethBlue: "#0acae6",
+        sixthBlue: "##0ddff0",
+        seventhBlue: "#0ff5fa",
+      },
+    },
   },
   plugins: [],
 };
